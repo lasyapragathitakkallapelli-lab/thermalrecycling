7ade23fce2a55a8d6329d8409be3dbd7441a15a2
lasyapragathi <lasyapragathitakkallapelli@gmail.com>
2026-04-08 15:38:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-import streamlit as st
-import streamlit.components.v1 as components
 
-# read the HTML file
-with open("index.html", "r", encoding="utf-8") as f:
-    html = f.read()
-
-components.html(html, height=900, scrolling=True)
